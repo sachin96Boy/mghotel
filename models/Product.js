3c70fa77f02253e1abe196547ec921a563b83f6d
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-05 16:42:45 +0530

    update: api on products seed ad show unctions

--- models/Product.js
+++ models/Product.js
@@ -15,6 +15,6 @@ const productSchema = new mongoose.Schema({
     timestamps: true,
 });
 
-const Product = mongoose.model("Product", productSchema);
+const Product =  mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
